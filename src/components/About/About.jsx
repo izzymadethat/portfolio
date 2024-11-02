05b40c71f6f2db0f2aca87880a1c65f306a1defd
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-11-02 15:41:51 -0400

    build about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,41 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h3 className="title">About Me</h3>
+      <div className="about about__bio">
+        <div>
+          <p>
+            Hello, and thanks for traveling to my space! I'm Isaiah Vickers. My
+            programming adventure launched on June 8th, 2023.
+          </p>
+          <p>
+            Before I became a programmer, I was (and still am) a professional
+            audio engineer. I decided to dive into the world of code because I
+            love solving puzzles and exploring new frontiers. Becoming a
+            software engineer felt like the perfect mission for me.
+          </p>
+          <p>
+            Coding has let me use all the skills I've picked up over the years.
+            I have experience in design, finance, marketing, and music. Now, I
+            get to mix all that knowledge with my love for being a programmer.
+          </p>
+        </div>
+        <div>
+          <p>
+            Discovering new techniques and languages is like discovering new
+            planets, except we&apos;re in the vast universe of technology! This
+            has opened up so many exciting opportunities for me, and it's the
+            best decision I've ever made!
+          </p>
+          <p>
+            Join me as I journey through the cosmos of code, exploring new
+            worlds and creating amazing things along the way.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
