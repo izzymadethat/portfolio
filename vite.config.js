1f52ac1df771a9dd07835ac40275463d95bebc01
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-11-03 01:11:10 -0400

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/",
+  base: "/",
   plugins: [
     react(),
     eslint({
